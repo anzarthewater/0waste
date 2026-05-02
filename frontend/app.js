f329bdbe40a69e0d519046f51090a1a50c5162b8
anzarthewater <chromiehommie69@gmail.com>
2026-05-02 11:26:23 -0400

    fix js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-fetch('https://zerowaste-backend-wlls.onrender.com')
+fetch('https://zerowaste-backend-wlls.onrender.com/api/hello')
   .then(res => res.json())
   .then(data => {
     document.getElementById('message').textContent = data.message;
